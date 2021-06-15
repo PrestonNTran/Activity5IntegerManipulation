199304cfdd2d6d31cbe9bcc285cdacf17959f2ff
Kyle Hekkers <kyle.hekkers@gmail.com>
2021-06-15 14:28:13 -0500

    Working Loop

--- Activity5/Activity5/Program.cs
+++ Activity5/Activity5/Program.cs
@@ -6,7 +6,48 @@ namespace Activity5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bool doLoop = true;
+            while (doLoop)
+            {
+                Console.WriteLine("Select An Option:");
+                Console.WriteLine("1: Display the whole list of input");
+                Console.WriteLine("2: Display every even number");
+                Console.WriteLine("3: Display every odd number");
+                Console.WriteLine("4: Display every 5th number");
+                Console.WriteLine("5: Display the sum of the integers");
+                Console.WriteLine("6: Display every number divisible by 3");
+                Console.WriteLine("7: Quit");
+
+                Int32 input = Int32.Parse(Console.ReadLine());
+
+
+                switch (input)
+                {
+                    case 1:
+                        
+                        break;
+                    case 2:
+
+                        break;
+                    case 3:
+
+                        break;
+                    case 4:
+
+                        break;
+                    case 5:
+
+                        break;
+                    case 6:
+
+                        break;
+                    case 7:
+                        doLoop = false;
+                        break;
+                }
+
+            }
+            Console.WriteLine("Loop Closed");
         }
     }
 }
